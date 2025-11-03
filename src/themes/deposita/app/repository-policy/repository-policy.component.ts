fbea1ac76a825ab133a87c3c57d6350d5b5fa657
Gabriel Marcos <gabrielmarcosbguimaraes@gmail.com>
2025-11-03 10:48:00 -0300

    adicionando conteúdo nas páginas estáticas

--- src/themes/deposita/app/repository-policy/repository-policy.component.ts
+++ src/themes/deposita/app/repository-policy/repository-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'repository-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './repository-policy.component.html',
   styleUrls: ['./repository-policy.component.scss']
 })
