be3eff7c4c6582fea45695e6741824cead8d83fa
Gabriel Marcos <gabrielmarcosbguimaraes@gmail.com>
2025-11-19 11:16:53 -0300

    ajustando layout do menu navegar

--- src/themes/deposita/app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.ts
+++ src/themes/deposita/app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.ts
@@ -7,11 +7,10 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ComcolPageBrowseByComponent as BaseComponent } from '../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component';
 
 @Component({
-  selector: 'ds-themed-comcol-page-browse-by',
-  // styleUrls: ['./comcol-page-browse-by.component.scss'],
-  styleUrls: ['../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.scss'],
-  // templateUrl: './comcol-page-browse-by.component.html'
-  templateUrl: '../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.html',
+ styleUrls: ['./comcol-page-browse-by.component.scss'],
+  // styleUrls: ['../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.scss'],
+  templateUrl: './comcol-page-browse-by.component.html',
+  // templateUrl: '../../../../../../app/shared/comcol/comcol-page-browse-by/comcol-page-browse-by.component.html',
   standalone: true,
   imports: [
     AsyncPipe,
